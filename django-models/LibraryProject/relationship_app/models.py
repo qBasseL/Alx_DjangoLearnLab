314822c27be790c41b7a2990fa13cd247de54549
Bassel <basselalaa17@gmail.com>
2025-07-27 19:21:37 +0300

    done

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,44 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# Author model
 class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+# Library model
+class Library(models.Model):
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+# Book model with ForeignKey and custom permissions
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
-	class Meta:
+    def __str__(self):
+        return self.title
+
+    class Meta:
         permissions = [
             ("can_add_book", "Can add book"),
             ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
         ]
-	
-    def __str__(self):
-        return self.title
 
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
+# Librarian model (OneToOne relationship with User and ForeignKey to Library)
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
-from django.contrib.auth.models import User
-from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# Member model (OneToOne with User and ManyToMany to Book)
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    borrowed_books = models.ManyToManyField(Book, blank=True)
 
+    def __str__(self):
+        return self.user.username
+
+# UserProfile for role-based access
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -52,22 +66,9 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
+# Automatically create UserProfile when User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance, role='Member')  # Default role
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    published_date = models.DateField()
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
-    def __str__(self):
-        return self.title
