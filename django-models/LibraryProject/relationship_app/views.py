bc0bf2695cb6cf7276cea39a26af17a5a542430e
Bassel <basselalaa17@gmail.com>
2025-07-27 18:04:07 +0300

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,12 @@ from .models import Book, Library
 # ✅ Function-Based View: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # ✅ Class-Based View: Show library detail
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
 
