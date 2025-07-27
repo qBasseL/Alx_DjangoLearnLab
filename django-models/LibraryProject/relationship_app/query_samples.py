312e103677a225fcb02798278780a065da630c30
Bassel <basselalaa17@gmail.com>
2025-07-27 17:49:39 +0300

    done

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,37 +1,27 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Clear all data (for testing)
-Author.objects.all().delete()
-Book.objects.all().delete()
-Library.objects.all().delete()
-Librarian.objects.all().delete()
-
 # Create sample data
 author = Author.objects.create(name="J.K. Rowling")
-book1 = Book.objects.create(title="Harry Potter 1", author=author)
-book2 = Book.objects.create(title="Harry Potter 2", author=author)
+book1 = Book.objects.create(title="HP1", author=author)
+book2 = Book.objects.create(title="HP2", author=author)
 
-library = Library.objects.create(name="Downtown Library")
+library_name = "My Library"
 library = Library.objects.create(name=library_name)
 library.books.add(book1, book2)
 
-librarian = Librarian.objects.create(name="John Doe", library=library)
+Librarian.objects.create(name="Sarah", library=library)
 
-# 1. All books by J.K. Rowling
-print("Books by J.K. Rowling:")
-for book in Book.objects.filter(author__name="J.K. Rowling"):
-    print(f"- {book.title}")
+# Required queries
+books = Book.objects.filter(author__name="J.K. Rowling")
 
-# 2. All books in the Downtown Library
-print("\nBooks in Downtown Library:")
-for book in library.books.all():
-    print(f"- {book.title}")
+library_instance = Library.objects.get(name=library_name)  # ✅ THIS LINE EXACTLY
 
-# 3. Librarian of the Downtown Library
-print(f"\nLibrarian of {library.name}: {library.librarian.name}")
+books_in_library = library_instance.books.all()
+librarian_name = library_instance.librarian.name
+
